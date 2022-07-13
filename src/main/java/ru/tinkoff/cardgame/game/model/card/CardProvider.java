89c7c660a23b306b0ea66f4ca9e4b46545b93410
FeNix784 <fenix784@mail.ru>
2022-07-13 19:21:22 +0300

    Изменил способности у некоторых карт.

--- src/main/java/ru/tinkoff/cardgame/game/model/card/CardProvider.java
+++ src/main/java/ru/tinkoff/cardgame/game/model/card/CardProvider.java
@@ -32,11 +32,11 @@ public enum CardProvider {
         cards.add(new Card("Гундыр", 3, "Чудовищный многоголовый великан, имеющий человеческий облик.", 4, 4, Spell.SPELL9, 3, CardClass.Giant));
         cards.add(new Card("Хрунгнир", 3, "Сильнейший великан в каменных доспехах.", 5, 6, Spell.SPELL16, 4, CardClass.Giant));
         cards.add(new Card("Атлант", 3, "Великан, который держит небо.", 5, 8, Spell.SPELL7, 5, CardClass.Giant));
-        cards.add(new Card("Механический голем", 3, "Огромная махина из металла и магии, которая медленно, но верно идет к своей цели.", 1, 4, Spell.SPELL1, 1, CardClass.Machine));
-        cards.add(new Card("Механический танк", 3, "Большое механизированное устройство, напоминающее робота и управляемое человеком изнутри.", 2, 3, Spell.SPELL1, 2, CardClass.Machine));
-        cards.add(new Card("Железнозубый прыгун", 3, "Воплощение саблезубого тигра в металле, которое также боится воды.", 4, 3, Spell.SPELL1, 3, CardClass.Machine));
-        cards.add(new Card("Безумный бот", 3, "Огромный робот, имеющий человекоподобный облик и управляемый компьютером.", 3, 5, Spell.SPELL1, 4, CardClass.Machine));
-        cards.add(new Card("Кошмарное слияние", 3, "Это существо, управляемое механизмами, одновременно и ведьма, и дракон, и демон, и великан.", 6, 9, Spell.SPELL1, 5, CardClass.Machine));
+        cards.add(new Card("Механический голем", 3, "Огромная махина из металла и магии, которая медленно, но верно идет к своей цели.", 1, 4, Spell.SPELL21, 1, CardClass.Machine));
+        cards.add(new Card("Механический танк", 3, "Большое механизированное устройство, напоминающее робота и управляемое человеком изнутри.", 2, 3, Spell.SPELL2, 2, CardClass.Machine));
+        cards.add(new Card("Железнозубый прыгун", 3, "Воплощение саблезубого тигра в металле, которое также боится воды.", 4, 3, Spell.SPELL6, 3, CardClass.Machine));
+        cards.add(new Card("Безумный бот", 3, "Огромный робот, имеющий человекоподобный облик и управляемый компьютером.", 3, 5, Spell.SPELL19, 4, CardClass.Machine));
+        cards.add(new Card("Кошмарное слияние", 3, "Это существо, управляемое механизмами, одновременно и ведьма, и дракон, и демон, и великан.", 6, 9, Spell.SPELL16, 5, CardClass.Machine));
     }
 
     public CopyOnWriteArrayList<Card> getCards() {
